2b47fdf6a58080e0e152a2e5076ec88dda35ff84
linyjj <145088346+linyjj@users.noreply.github.com>
2024-11-20 11:56:52 +0800

    week11

---
+++ week11/week11-1.py
@@ -0,0 +1,7 @@
+table={}
+table[9977341]=9
+table[4433997]=4
+print(table[9977341])
+print(table[4433997])
+if 0 not in table:
+  print("table[0]¤£¦s¦b")
---
+++ week11/week11-2.py
@@ -0,0 +1,7 @@
+from typing_extensions import Counter
+nums=[1,1,1,2,3]
+from collections import Counter
+counter=Counter()
+for num in nums:
+  counter[num]+=1
+print(counter)
